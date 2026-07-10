07aeb63b58d82236b768ff2ec7a62c11893d5312
Vinícius Nascimento <78743170+viniNascimento63@users.noreply.github.com>
2026-07-10 14:49:50 +0000

    adiciona scripts `lint:check` e `lint:fix`

--- pages/index.js
+++ pages/index.js
@@ -1,5 +1,10 @@
 function Home() {
-    return <h1>Hevelin, doce mulher, esposa querida. Com você minha vida torna-se mais divertida!</h1>
+  return (
+    <h1>
+      Hevelin, doce mulher, esposa querida. Com você minha vida torna-se mais
+      divertida!
+    </h1>
+  );
 }
 
 export default Home;
