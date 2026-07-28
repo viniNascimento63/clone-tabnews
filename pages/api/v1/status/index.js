762f633da92cbfc837dba09c2043aab2d0b9035f
Vinícius Nascimento <78743170+viniNascimento63@users.noreply.github.com>
2026-07-28 18:03:21 +0000

    /status retorna versão do banco e data + testes

--- pages/api/v1/status/index.js
+++ pages/api/v1/status/index.js
@@ -1,9 +1,22 @@
 import database from "/infra/database";
 
 async function status(request, response) {
-  const result = await database.query("SELECT 1 + 1 as sum;");
-  console.log(result.rows);
-  response.status(200).json({ chave: "valor" });
+  const updatedAt = new Date().toISOString();
+  const dbInfo = await database.query(
+    "SELECT setting FROM pg_settings WHERE name = 'server_version';",
+  );
+  const dbVersion = dbInfo.rows[0]["setting"];
+
+  response.status(200).json({
+    updated_at: updatedAt,
+    dependencies: {
+      databse: {
+        max_connections: null,
+        opened_connections: null,
+        version: dbVersion,
+      },
+    },
+  });
 }
 
 export default status;
