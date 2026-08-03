c5adf19490e0b44dfc4eb64d5622aa713ae44436
Vinícius Nascimento <78743170+viniNascimento63@users.noreply.github.com>
2026-08-03 14:59:45 +0000

    fix: corrige número conexões abertas

--- pages/api/v1/status/index.js
+++ pages/api/v1/status/index.js
@@ -3,7 +3,7 @@ import database from "/infra/database";
 async function status(request, response) {
   const updatedAt = new Date().toISOString();
   const dbInfo = await database.query(
-    "SELECT setting FROM pg_settings WHERE name = 'server_version' UNION ALL SELECT setting AS max_connections FROM pg_settings WHERE name = 'max_connections' UNION ALL SELECT count(*)::text as opened_connections FROM pg_stat_activity;",
+    "SELECT setting FROM pg_settings WHERE name = 'server_version' UNION ALL SELECT setting AS max_connections FROM pg_settings WHERE name = 'max_connections' UNION ALL SELECT count(*)::text as opened_connections FROM pg_stat_activity WHERE datname = 'local_db';",
   );
   const dbVersion = dbInfo.rows[0]["setting"];
   const dbMaxConnections = parseInt(dbInfo.rows[1]["setting"]);
