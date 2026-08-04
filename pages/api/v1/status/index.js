bf40fe82dcdc490edbb827d6370af511f356ea1a
Vinícius Nascimento <78743170+viniNascimento63@users.noreply.github.com>
2026-08-04 14:46:01 +0000

    refactor: legibilidade

--- pages/api/v1/status/index.js
+++ pages/api/v1/status/index.js
@@ -2,20 +2,28 @@ import database from "/infra/database";
 
 async function status(request, response) {
   const updatedAt = new Date().toISOString();
-  const dbInfo = await database.query(
-    "SELECT setting FROM pg_settings WHERE name = 'server_version' UNION ALL SELECT setting AS max_connections FROM pg_settings WHERE name = 'max_connections' UNION ALL SELECT count(*)::text as opened_connections FROM pg_stat_activity WHERE datname = 'local_db';",
+
+  const dbVersion = await database.query("SHOW server_version");
+  const dbVersionValue = dbVersion.rows[0].server_version;
+
+  const dbMaxConnections = await database.query("SHOW max_connections");
+  const dbMaxConnectionsValue = parseInt(
+    dbMaxConnections.rows[0].max_connections,
   );
-  const dbVersion = dbInfo.rows[0]["setting"];
-  const dbMaxConnections = parseInt(dbInfo.rows[1]["setting"]);
-  const dbOpenedConnections = parseInt(dbInfo.rows[2]["setting"]);
+  const dbOpenedConnections = await database.query(
+    "SELECT count(*)::int as opened_connections FROM pg_stat_activity WHERE datname = 'local_db';",
+  );
+  const dbOpenedConnectionsValue =
+    dbOpenedConnections.rows[0].opened_connections;
+  console.log(dbOpenedConnectionsValue);
 
   response.status(200).json({
     updated_at: updatedAt,
     dependencies: {
       database: {
-        max_connections: dbMaxConnections,
-        opened_connections: dbOpenedConnections,
-        version: dbVersion,
+        max_connections: dbMaxConnectionsValue,
+        opened_connections: dbOpenedConnectionsValue,
+        version: dbVersionValue,
       },
     },
   });
